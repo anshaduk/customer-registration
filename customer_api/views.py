58aecacdf5180634e9b8d3b96e235da1d40b9282
Anshad UK <anshadu@gmail.com>
2025-02-18 22:10:35 +0530

    define all functionalities

--- customer_api/views.py
+++ customer_api/views.py
@@ -38,3 +38,42 @@ class PersonalDetailDetailView(APIView):
         personal_detail = self.get_object(pk)
         personal_detail.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class EmploymentDetailList(APIView):
+    def get(self,request):
+        employment_details = EmploymentDetail.objects.all()
+        serializer = EmploymentDetailSerializer(employment_details,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer= EmploymentDetailSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class EmploymentDetailDetialView(APIView):
+    def get_object(self,pk):
+        try:
+            return EmploymentDetail.objects.get(pk=pk)
+        except EmploymentDetail.DoesNotExist:
+            return Response({"detail":"Employement details not found with the given id."})
+        
+    def get(self,request,pk):
+        employement_detail = self.get_object(pk)
+        serializer = EmploymentDetailSerializer(employement_detail)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        employement_detail = self.get_object(pk)
+        serializer = EmploymentDetailSerializer(employement_detail,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+        employment_detail = self.get_object(pk)
+        employment_detail.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
